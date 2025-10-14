2a759176b8712a90d4bf192b770d17c75852e22c
Quantum Daniel <aburorhambagadaniel@gmail.com>
2025-10-14 09:21:12 +0100

    add  two pages and tool tip on admission form

---
+++ check-admission.js
@@ -0,0 +1,4 @@
+document.querySelector('.admission-button').addEventListener('click',()=>{
+    document.querySelector('.admission-content').innerHTML = 'A message will be sent to you containing your admision status...'
+
+})
